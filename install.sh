c615471a49079558f3d5ff26fd6f208a32186485
Eli Smith <plunkinguitar@gmail.com>
2016-04-19 09:26:27 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,15 +37,17 @@ echo "Creating working directories..."
 sudo mkdir -p $LIB_PATH $CONFIG_PATH $CONFIG_PATH/meta $CRI_DIR $DEST_DIR $PKG_PATH # Create new directories
 sudo chown -R $USER:$USER $LIB_PATH $CONFIG_PATH $CONFIG_PATH/meta $CRI_DIR $DEST_DIR $PKG_PATH # Change ownership
 
-cd $DEST_DIR
 PKGURL=$URL/chromelib
 
+cd $CRI_DIR
 printf "\nDownloading CRI files\n\n..." 
 sudo wget -q --no-check-certificate "$URL/installLIST.txt" -O $CRI_DIR/installLIST.txt #This is to download list of files needed
 sudo chmod 755 installLIST.txt #Makes the commands file have every permisson so that anyone can use it 
 NAMES="$(< installLIST.txt)" #names from names.txt file
 LINES=$(lineCount)
 NUMBERS=1
+
+cd $DEST_DIR
 
 for NAME in $NAMES; do #Downloads all nessisary files from github to /usr/local/bin
     clean
