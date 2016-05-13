c12e626bc9944fb5a2fd3ed0d070a7128862e30e
Eli Smith <plunkinguitar@gmail.com>
2016-05-13 15:40:32 -0500

    Final fixes to icons

--- localSite/js/run.js
+++ localSite/js/run.js
@@ -21,7 +21,9 @@ ws.onmessage = function(str) {
     nameProgs = progs.split("\n");
     for (i=0;i<nameProgs.length;i++) {
         nameProgs[i] = nameProgs[i].split(" : ");
-
+        if (nameProgs[i][2] == (null || "")) {
+            nameProgs[i][2] = "defIcon";
+        }
         document.getElementById('items').innerHTML += "<img height='50' width='50' src='images/icons/"+nameProgs[i][2]+".png'><button onclick='progClicked(this.id)' class='item' id='"+nameProgs[i][0]+"'>"+nameProgs[i][1]+"</button>";
     }
 } 
