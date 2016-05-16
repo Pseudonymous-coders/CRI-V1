039cd19c5b1264834e522e95a358996a5c1471d8
Eli Smith <plunkinguitar@gmail.com>
2016-05-16 15:31:58 -0500

    Timer for updateing

--- localSite/js/settings.js
+++ localSite/js/settings.js
@@ -35,7 +35,7 @@ ws.onmessage = function(str) {
         document.getElementById('rel').innerHTML = curRel;
         document.getElementById('dat').innerHTML = curDat;
     }else if (str.data.substring(0, 8) == "UPDATING") {
-        document.getElementById('update-stat').innerHTML = "Updating CRI, please wait";
+        console.log("updating");
     }
 }
 
@@ -50,6 +50,9 @@ function stopAll() {
 
 function update() {
     document.getElementById('cover').style.visibility = "visible";
+    document.getElementById('update-stat').style.visibility = "visible";
+    display = document.querySelector('#timer');
+    startTimer(4*60, display);
     ws.send("UPDATE");
 }
 
@@ -64,3 +67,21 @@ $(".cool").click(function() {
 $(".danger").click(function() {
     $(".danger").toggleClass("danger-active");
 });
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
