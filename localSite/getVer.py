4022d157fe6aa67db475c8ceb99f6ac62494338c
Eli Smith <plunkinguitar@gmail.com>
2016-05-16 08:50:20 -0500

    Fixes to CB Check

--- localSite/getVer.py
+++ localSite/getVer.py
@@ -1,6 +1,11 @@
 def ver():
     import re, urllib2
-    f = open("/root/Downloads/.tmp/cridate/cri.cfg")
+    from subprocess import Popen, PIPE
+    CB = int(Popen("if [ -s /usr/local/bin/xiwi ]; then echo 1; else echo 0; fi", stdout=PIPE, shell=True).communicate()[0]) 
+    if not CB:
+        f = open("/home/eli/Downloads/.tmp/cridate/cri.cfg")
+    else:
+        f = open("/root/Downloads/.tmp/cridate/cri.cfg")
     current = f.read()
     versions = current
     latest = urllib2.urlopen("https://raw.githubusercontent.com/Pseudonymous-coders/CRI/master/globs/cri.cfg").read()
